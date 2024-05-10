893c7232668989d1947c0bcad0b57038cf3bbf0e
maram <maramguenouatii@gmail.com>
2024-05-10 20:08:07 +0100

    maram

---
+++ src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,16 @@
+export const EnvironmentalVariables = {
+    production:false,
+
+    //Base Urls to access the end points using the http client get method
+    weatherApiLocationBaseURL:'https://weather338.p.rapidapi.com/locations/search?',
+    weatherApiForecastBaseURL:'https://weather338.p.rapidapi.com/weather/forecast?',
+    
+    //Variables for API key name and value
+    xRapidApiKeyName:'X-RapidAPI-Key',
+    xRapidApikeyValue:'cf2a9baee6mshfa86a8d9438fa72p1b1bd5jsnc7a31e4cbefe',
+
+    //Variables for host name and value
+    xRapidApiHostName:'X-RapidAPI-Host',
+    xRapidApiHostValue:'weather338.p.rapidapi.com'
+    
+}
